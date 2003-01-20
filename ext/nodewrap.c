5a4d834fef4e55f34e100b86762da149897f6a53
cout <cout>
2003-01-20 17:41:59 +0000

    Moved extension files to the ext directory. Added inline Rdoc documentation.

--- ext/nodewrap.c
+++ ext/nodewrap.c
@@ -10,16 +10,24 @@ static VALUE rb_cNode = Qnil;
 static VALUE rb_mMarshal;
 static VALUE node_dump_fmt = Qnil;
 
-// ---------------------------------------------------------------------
-// Node methods
+/* ---------------------------------------------------------------------
+ * Node methods
+ * ---------------------------------------------------------------------
+ */
 
+/*
+ * Allocate a new node
+ */
 static VALUE node_allocate(VALUE klass)
 {
   NODE * n = NEW_NIL();
-  // TODO: Need a free function in this case
+  /* TODO: Need a free function in this case */
   return Data_Wrap_Struct(rb_cNode, rb_gc_mark, 0, n);
 }
 
+/*
+ * Returns a node's flags
+ */
 static VALUE node_flags(VALUE self)
 {
   NODE * n;
@@ -27,6 +35,9 @@ static VALUE node_flags(VALUE self)
   return INT2NUM(n->flags);
 }
 
+/*
+ * Returns the file the node is associated with
+ */
 static VALUE node_nd_file(VALUE self)
 {
   NODE * n;
@@ -34,6 +45,9 @@ static VALUE node_nd_file(VALUE self)
   return rb_str_new2(n->nd_file);
 }
 
+/*
+ * Returns an integer representing the type of the node
+ */
 static VALUE node_nd_type(VALUE self)
 {
   NODE * n;
@@ -64,6 +78,10 @@ static VALUE add_method(VALUE klass, VALUE method, VALUE node, VALUE noex)
   return Qnil;
 }
 
+/*
+ * Given a Method, returns the Node for that Method.  This can be used
+ * to directly copy one class's method to another (using add_method).
+ */
 static VALUE method_node(VALUE self, VALUE method)
 {
   struct METHOD * m;
@@ -71,8 +89,10 @@ static VALUE method_node(VALUE self, VALUE method)
   return Data_Wrap_Struct(rb_cNode, rb_gc_mark, 0, m->body);
 }
 
-// ---------------------------------------------------------------------
-// Marshalling
+/* ---------------------------------------------------------------------
+ * Marshalling
+ * ---------------------------------------------------------------------
+ */
 
 VALUE marshal_dump(VALUE obj, VALUE limit)
 {
@@ -84,8 +104,10 @@ VALUE marshal_load(VALUE obj)
   return rb_funcall(rb_mMarshal, rb_intern("load"), 1, obj);
 }
 
-// ---------------------------------------------------------------------
-// Node marshalling
+/* ---------------------------------------------------------------------
+ * Node marshalling
+ * ---------------------------------------------------------------------
+ */
 
 void dump_node_to_hash(NODE * n, VALUE node_hash)
 {
@@ -140,8 +162,10 @@ void load_node_from_hash(NODE * n, VALUE orig_node_id, VALUE node_hash, VALUE id
   nd_file = STR2CSTR(rb_nd_file);
   nd_file_len = RSTRING(rb_nd_file)->len; 
 
-  // The id_hash is a temporary, so it is invalidated if an exception is
-  // raised.
+
+  /* The id_hash is a temporary, so it is invalidated if an exception is
+   * raised.
+   */
   rb_hash_aset(id_hash, orig_node_id, node_id(n));
 
   descrip = node_type_descrip((flags & FL_UMASK) >> FL_USHIFT);
@@ -149,14 +173,16 @@ void load_node_from_hash(NODE * n, VALUE orig_node_id, VALUE node_hash, VALUE id
   load_node_elem(descrip->n2, s2, &tmp_node, node_hash, id_hash);
   load_node_elem(descrip->n3, s3, &tmp_node, node_hash, id_hash);
 
-  // Note that the garbage collector CAN be invoked at this point, so
-  // any node object the GC knowns about must be in a consistent state.
+  /* Note that the garbage collector CAN be invoked at this point, so
+   * any node object the GC knowns about must be in a consistent state.
+   */
   nd_file_buf = ALLOC_N(char, nd_file_len + 1);
 
-  // 1) We must NOT get an exception from here on out, since we are
-  // modifying a live node, and so nd_file_buf won't be leaked.
-  // 2) We must NOT invoke the garbage collector from here on out, since
-  // we are modifying a live node.
+  /* 1) We must NOT get an exception from here on out, since we are
+   * modifying a live node, and so nd_file_buf won't be leaked.
+   * 2) We must NOT invoke the garbage collector from here on out, since
+   * we are modifying a live node.
+   */
   memcpy(n, &tmp_node, sizeof(NODE));
   n->flags = flags;
   n->nd_file = nd_file_buf;
@@ -178,10 +204,13 @@ static VALUE node_from_hash(VALUE klass, VALUE node_id, VALUE node_hash)
   NODE * n = NEW_NIL();
   VALUE id_hash = rb_hash_new();
   load_node_from_hash(n, node_id, node_hash, id_hash);
-  // TODO: Need a free function in this case
+  /* TODO: Need a free function in this case */
   return Data_Wrap_Struct(rb_cNode, rb_gc_mark, 0, n);
 }
 
+/*
+ * Dump a node
+ */
 static VALUE node_dump(VALUE self, VALUE limit)
 {
   NODE * n;
@@ -193,6 +222,9 @@ static VALUE node_dump(VALUE self, VALUE limit)
   return marshal_dump(arr, limit);
 }
 
+/*
+ * Load a dumped node
+ */
 static VALUE node_load(VALUE klass, VALUE str)
 {
   NODE * n = NEW_NIL();
@@ -201,12 +233,14 @@ static VALUE node_load(VALUE klass, VALUE str)
   VALUE node_id = rb_ary_pop(arr);
   VALUE id_hash = rb_hash_new();
   load_node_from_hash(n, node_id, node_hash, id_hash);
-  // TODO: Need a free function in this case
+  /* TODO: Need a free function in this case */
   return Data_Wrap_Struct(rb_cNode, rb_gc_mark, 0, n);
 }
 
-// ---------------------------------------------------------------------
-// Class marshalling
+/* ---------------------------------------------------------------------
+ * Class marshalling
+ * ---------------------------------------------------------------------
+ */
 
 static char const * insert_module_sorted_str = 
   "proc { |modules, m|\n"
@@ -335,7 +369,7 @@ static VALUE superclass_name(VALUE module)
 
 static VALUE class_variable_hash(VALUE module)
 {
-  return rb_hash_new(); // TODO
+  return rb_hash_new(); /* TODO */
 }
 
 static VALUE module_dump(VALUE self, VALUE limit)
@@ -387,7 +421,7 @@ static void add_method_iter(VALUE name, VALUE value, VALUE module)
 {
   NODE * n;
   rb_check_type(name, T_SYMBOL);
-  // TODO: Check that this is a node
+  /* TODO: Check that this is a node */
   Data_Get_Struct(value, NODE, n);
   rb_add_method(module, SYM2ID(name), n->nd_body, n->nd_noex);
 }
@@ -424,7 +458,7 @@ static void add_constants(VALUE module, VALUE constants)
 static void add_class_variables(VALUE module, VALUE class_variables)
 {
   rb_check_type(class_variables, T_HASH);
-  // TODO
+  /* TODO */
 }
 
 static VALUE module_load(VALUE klass, VALUE str)
@@ -466,28 +500,30 @@ static VALUE module_load(VALUE klass, VALUE str)
   return module;
 }
 
-// ---------------------------------------------------------------------
-// Initialization
+/* ---------------------------------------------------------------------
+ * Initialization
+ * ---------------------------------------------------------------------
+ */
 
 void Init_nodewrap(void)
 {
   rb_cNode = rb_define_class("Node", rb_cObject);
   rb_mMarshal = rb_const_get(rb_cObject, rb_intern("Marshal"));
 
-  // TODO: should be a private class method
+  /* TODO: should be a private class method */
   rb_define_singleton_method(rb_cNode, "allocate", node_allocate, 0);
 
   rb_define_method(rb_cNode, "flags", node_flags, 0);
   rb_define_method(rb_cNode, "nd_file", node_nd_file, 0);
   rb_define_method(rb_cNode, "nd_type", node_nd_type, 0);
 
-  rb_define_method(rb_cNode, "to_hsh", node_to_hash, 0);
-  rb_define_singleton_method(rb_cNode, "from_hsh", node_from_hash, 1);
-
   rb_define_method(rb_cNode, "_dump", node_dump, 1);
   rb_define_singleton_method(rb_cNode, "_load", node_load, 1);
 
   rb_define_singleton_method(rb_cNode, "method_node", method_node, 1);
+
+  // VALUE rb_cModule = rb_define_class("Module", rb_cObject);
+  VALUE rb_cModule = rb_const_get(rb_cObject, rb_intern("Module"));
   rb_define_method(rb_cModule, "add_method", add_method, 3);
 
   rb_define_method(rb_cModule, "_dump", module_dump, 1);
