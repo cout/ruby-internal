0387fb8d9b8fdae4ad32758e4bd286b0b12aaad5
cout <cout>
2004-07-30 02:05:53 +0000

    Added a check to make sure the ruby source code can be found at the location specified.

--- post-config.rb
+++ post-config.rb
@@ -1,6 +1,15 @@
 if not @config['ruby-source-path'] then
-  $stderr.puts "Source code for the ruby interpreter could not be found (perhaps you forgot --ruby-source-path?)"
+  $stderr.puts "ERROR: Source code for the ruby interpreter could not be found (perhaps you forgot --ruby-source-path?)"
   exit 1
+end
+
+path=@config['ruby-source-path']
+ruby_h=File.join(path, 'ruby.h')
+if not File.exist?(path) or
+   not File.directory?(path) or
+   not File.exist?(ruby_h) then
+   $stderr.puts "ERROR: Could not find the ruby source code at #{path}"
+   exit 1
 end
 
 dive_into('ext') do
