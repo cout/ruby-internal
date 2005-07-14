5be2631dc82ae85288229dcb70d6c70f018eb8bc
cout <cout>
2005-07-14 13:09:35 +0000

    Added many new node types.  Also added documentation and code to check that the node type exists before defining an as_expression method for that node type.

--- lib/as_expression.rb
+++ lib/as_expression.rb
@@ -1,39 +1,53 @@
 class Node
-  class LIT < Node
-    def as_expression
-      return self.lit.to_s
+  # Return an string describing this node as a single expression.  By
+  # default, this just returns the name of the node's type, but some
+  # node types override this method to produce more meaningful output.
+  def define_expression(klass, &block)
+    if defined(:klass) then
+      const_get(klass).define_method(:as_expression, &block)
     end
   end
 
-  class FCALL < Node
-    def as_expression
-      args = self.args
-      return "#{self.mid}(#{args ? args.as_expression(false) : ''})"
-    end
+  define_expression(:LIT) do
+    return self.lit.to_s
+  end
+
+  define_expression(:FCALL) do
+    args = self.args
+    return "#{self.mid}(#{args ? args.as_expression(false) : ''})"
+  end
+
+  define_expression(:VCALL) do
+    args = self.args
+    return "#{self.mid}(#{args ? args.as_expression(false) : ''})"
   end
 
   @@arithmetic_expressions = [
-    :+, :-, :*, :/, :<, :>, :<=, :>=, :==, :===,
-    '!='.intern, '~='.intern, ':!~'.intern
+    :+, :-, :*, :/, :<, :>, :<=, :>=, :==, :===, :<=>, :<<, :>>, :&, :|,
+    :!, :^, :%, '!='.intern, '~='.intern, ':!~'.intern
   ]
 
-  class CALL < Node
-    def as_expression
-      case self.mid
-      when *@@arithmetic_expressions
-        args = self.args
-        return "#{self.recv.as_expression} #{self.mid} #{args ?  args.as_expression(false) : ''}"
-      else
-        args = self.args
-        return "#{self.recv.as_expression}.#{self.mid}(#{args ?  args.as_expression(false) : ''})"
-      end
+  define_expression(:CALL) do
+    case self.mid
+    when *@@arithmetic_expressions
+      args = self.args
+      return "#{self.recv.as_expression} #{self.mid} #{args ? args.as_expression(false) : ''}"
+    else
+      args = self.args
+      return "#{self.recv.as_expression}.#{self.mid}(#{args ? args.as_expression(false) : ''})"
     end
   end
 
-  class VCALL < Node
-    def as_expression
-      return self.mid.to_s
-    end
+  define_expression(:VCALL) do
+    return self.mid.to_s
+  end
+
+  define_expression(:ZSUPER) do
+    return "super"
+  end
+
+  define_expression(:SUPER) do
+    return "super(#{args ? args.as_expression(false) : ''})"
   end
 
   class ARRAY < Node
@@ -47,7 +61,7 @@ class Node
       return a
     end
 
-    def as_expression(brackets=true)
+    def as_expression(brackets = false)
       s = brackets ? '[' : ''
       s += self.to_a.map { |n| n.as_expression }.join(', ')
       s += brackets ? ']' : ''
@@ -55,49 +69,96 @@ class Node
     end
   end
 
-  class HASH < Node
-    def as_expression
-      a = self.head.to_a
-      elems = []
-      i = 0
-      while i < a.size do
-        elems << "#{a[i].as_expression}=>#{a[i+1].as_expression}"
-        i += 2
-      end
-      return "{#{elems.join(', ')}}"
+  class ZARRAY < Node
+    def to_a
+      return []
+    end
+
+    def as_expression(brackets = false)
+      return brackets ? '[]' : ''
     end
   end
 
-  class IF < Node
-    def as_expression
-      return "#{self.cond.as_expression} ? " +
-             "#{self.body.as_expression} : " +
-             "#{self.else.as_expression}"
+  define_expression(:HASH) do
+    a = self.head.to_a
+    elems = []
+    i = 0
+    while i < a.size do
+      elems << "#{a[i].as_expression}=>#{a[i+1].as_expression}"
+      i += 2
+    end
+    return "{#{elems.join(', ')}}"
+  end
+
+  define_expression(:IF) do
+    return "#{self.cond.as_expression} ? " +
+           "#{self.body.as_expression} : " +
+           "#{self.else.as_expression}"
+  end
+
+  define_expression(:TRUENODE) do
+    return "true"
+  end
+
+  define_expression(:FALSENODE) do
+    return "false"
+  end
+
+  define_expression(:NILNODE) do
+    return "nil"
+  end
+
+  define_expression(:DOT2) do
+    return "#{self.beg.as_expression}..#{self.end.as_expression}"
+  end
+
+  define_expression(:DOT3) do
+    return "#{self.beg.as_expression}..#{self.end.as_expression}"
+  end
+
+  define_expression(:GVAR) do
+    return "#{self.vid}"
+  end
+
+  define_expression(:IVAR) do
+    return "#{self.vid}"
+  end
+
+  define_expression(:CVAR) do
+    return "#{self.vid}"
+  end
+
+  define_expression(:NODE_NTH_REF) do
+    return "$#{self.nth}"
+  end
+
+  define_expression(:ATTR_ASGN) do
+    case self.mid
+    when :[]=
+      args = self.args.to_a
+      attrs = args[1..-2].map { |n| n.as_expression }
+      value = args[-1].as_expression
+      return "#{self.recv.as_expression}[#{attrs.join(', ')}] = #{value}"
+    else
+      return "#{self.recv.as_expression}.#{self.mid}#{self.args.as_expression(false)}"
     end
   end
 
-  class TRUENODE < Node
-    def as_expression
-      return "true"
-    end
+  define_expression(:CONST) do
+    return "#{self.vid}"
   end
 
-  class FALSENODE < Node
-    def as_expression
-      return "false"
-    end
+  define_expression(:COLON2) do
+    return "#{self.mid}::#{self.value.as_expression}"
   end
 
-  class NILNODE < Node
-    def as_expression
-      return "nil"
-    end
+  define_expression(:NODE_LVAR) do
+    return "#{self.vid}"
   end
 
   def as_expression
     # default
-    return self.class
-    # return "<expression>"
+    return "<#{self.nd_type.to_s}>"
   end
 end
 
