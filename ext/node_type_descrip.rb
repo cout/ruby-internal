6247af9e6b785c005bee4abdf97221577bb45665
cout <cout>
2006-10-19 21:43:40 +0000

    Added CNT member to NODE_BACKREF.

--- ext/node_type_descrip.rb
+++ ext/node_type_descrip.rb
@@ -82,7 +82,7 @@ NODE_TYPE_DESCRIPS = [
   [ 'COLON2'      , 'HEAD'  , 'MID'   , 'NONE'  ], 
   [ 'COLON3'      , 'MID'   , 'NONE'  , 'NONE'  ], 
   [ 'NTH_REF'     , 'NTH'   , 'CNT'   , 'NONE'  ], 
-  [ 'BACK_REF'    , 'NTH'   , 'NONE'  , 'NONE'  ], 
+  [ 'BACK_REF'    , 'NTH'   , 'CNT'   , 'NONE'  ], 
   [ 'HASH'        , 'HEAD'  , 'NONE'  , 'NONE'  ], 
   [ 'ZARRAY'      , 'NONE'  , 'NONE'  , 'NONE'  ], 
   [ 'ARRAY'       , 'ALEN'  , 'HEAD'  , 'NEXT'  ], 
