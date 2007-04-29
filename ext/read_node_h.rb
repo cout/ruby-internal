c40b8d56e65d2c574d2f6dc20add1469916966de
cout <cout>
2007-04-29 11:34:40 +0000

    Print the contents of NODEINFO when run from the command line.

--- ext/read_node_h.rb
+++ ext/read_node_h.rb
@@ -5,13 +5,16 @@ NODE_H_LOCATION = "#{RUBY_SOURCE_DIR}/node.h"
 
 NODEINFO = Hash.new
 File.open(NODE_H_LOCATION) do |node_h|
-  loop do
-    line = node_h.gets
-    break if line.nil?
+  while line = node_h.gets do
     case line
     when /^#define\s+nd_(\w+)\s+(.*)/
       NODEINFO[$1] = $2
     end
   end
+end
+
+if __FILE__ == $0 then
+  require 'pp'
+  pp NODEINFO
 end
 
