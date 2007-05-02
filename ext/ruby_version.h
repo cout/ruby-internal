6238c613433790a57d6b8dc81c3a4f798634f7d4
cout <cout>
2007-05-02 19:46:14 +0000

    Fixed version checking.

--- ext/ruby_version.h
+++ ext/ruby_version.h
@@ -4,7 +4,7 @@
 #include "version.h"
 
 /* TODO: Not the best way to check this */
-#if RUBY_RELEASE_CODE >= 20070427
+#if RUBY_VERSION_CODE >= 190 && RUBY_RELEASE_CODE >= 20070427
 #define RUBY_HAS_YARV
 #endif
 
