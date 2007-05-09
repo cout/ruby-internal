68d95905272f03f26eb6025cd5e50dc4bd752011
cout <cout>
2007-05-09 11:08:49 +0000

    Removed dependency on rbconfig.

--- ext/ruby_version_code.rb
+++ ext/ruby_version_code.rb
@@ -1,7 +1,16 @@
-require 'rbconfig'
+require 'ruby_source_dir'
 
-major = Config::CONFIG['MAJOR'].to_i
-minor = Config::CONFIG['MINOR'].to_i
-teeny = Config::CONFIG['TEENY'].to_i
-RUBY_VERSION_CODE = major * 100 + minor * 10 + teeny
+# We could use rbconfig, but it might not be generated yet (or
+# correctly)
 
+File.open(File.join(RUBY_SOURCE_DIR, 'version.h')) do |version_h|
+  version_h.each_line do |line|
+    # #define RUBY_VERSION "1.6.4"
+    if line =~ /#define RUBY_VERSION \"(.*?)\.(.*?)\.(.*?)\"/ then
+      major = $1.to_i
+      minor = $2.to_i
+      teeny = $3.to_i
+      RUBY_VERSION_CODE = major * 100 + minor * 10 + teeny
+    end
+  end
+end
