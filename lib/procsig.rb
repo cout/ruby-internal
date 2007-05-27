5e030418af8ca0deff0406d66357be4fc57b475e
cout <cout>
2007-05-27 12:22:18 +0000

    Added YARV support.

--- lib/procsig.rb
+++ lib/procsig.rb
@@ -41,6 +41,15 @@ class Proc
     def rest_arg
       @rest_arg
     end
+
+    def empty_last_arg
+      if (@names.size == 1 and not @rest_arg) or # pre-YARV
+         (@rest_arg and @names[-1] == nil) then # YARV
+        return true
+      else
+        return false
+      end
+    end
   end
 
   # Return an Arguments object representing the arguments in the order
@@ -48,7 +57,8 @@ class Proc
   def arguments
     has_rest_arg = self.has_rest_arg
 
-    if self.var then
+    if self.respond_to?(:var) and self.var then
+      # pre-YARV
       case self.var
       when Node::DASGN_CURR
         return Arguments.new([ self.var.vid ], false, has_rest_arg ? 0 : nil)
@@ -68,6 +78,22 @@ class Proc
       else
         raise "Unexpected node type: #{self.var.class}"
       end
+    elsif
+      # YARV
+      iseq = self.body
+      local_vars = iseq.local_table
+      has_rest_arg = iseq.arg_rest != -1
+      has_block_arg = iseq.arg_block != -1
+      num_args = \
+        iseq.argc + \
+        iseq.arg_opt_table.size + \
+        (has_rest_arg ? 1 : 0) + \
+        (has_block_arg ? 1 : 0)
+      puts "argc=#{iseq.argc}, opt_table.size=#{iseq.arg_opt_table.size}"
+      puts "has_rest_arg=#{has_rest_arg}, has_block_arg=#{has_block_arg}"
+      names = local_vars[0...num_args]
+      # TODO: masgn
+      return Arguments.new(names, true, has_rest_arg ? -1 : nil)
     else
       return Arguments.new(nil, false, nil)
     end
@@ -75,13 +101,20 @@ class Proc
 
   # Return true if the proc has a rest arg
   def has_rest_arg
-    has_rest_arg = false
-    if self.var then
-      if self.var.class == Node::MASGN then
-        if self.var.args then
-          has_rest_arg = true
+    if self.respond_to?(:var) then
+      # pre-YARV
+      has_rest_arg = false
+      if self.var then
+        if self.var.class == Node::MASGN then
+          if self.var.args then
+            has_rest_arg = true
+          end
         end
       end
+    else
+      # YARV
+      rest = self.body.arg_rest
+      has_rest_arg = (rest >= 0 ? rest - 1 : nil)
     end
     return has_rest_arg
   end
@@ -99,7 +132,9 @@ class Proc
     # Rest arg
     if args.rest_arg then
       rest_name = args[args.rest_arg]
-      info[rest_name] = "*#{rest_name}"
+      if rest_name then
+        info[rest_name] = "*#{rest_name}"
+      end
     end
 
     return info
@@ -117,7 +152,7 @@ class Proc
       if @args.unspecified then
         return ""
       elsif @args.multiple_assignment then
-        if @args.size == 1 and not @args.rest_arg then
+        if @args.empty_last_arg then
           params = @args.map{ |n| arg_info[n] }
           return "|#{@arg_info[@args[0]]},|"
         else
