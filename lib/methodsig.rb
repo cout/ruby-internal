b6c520d845821559f3c82d6065f6ae1603af6d28
cout <cout>
2007-09-09 01:48:09 +0000

    Moved node-specific code into the node classes.

--- lib/methodsig.rb
+++ lib/methodsig.rb
@@ -47,64 +47,17 @@ module MethodSig
   end
 
   def local_vars
-    if self.body.respond_to?(:tbl) then
-      # pre-YARV
-      return self.body.tbl || []
-    else
-      # YARV
-      iseq = self.body.body
-      local_vars = iseq.local_table
-      return local_vars
-    end
+    return self.body.local_vars
   end
 
   # Return the names of the arguments this method takes, in the order in
   # which they appear in the argument list.
   def argument_names
-    local_vars = self.local_vars
-    if self.body.respond_to?(:tbl) then
-      # pre-YARV
-      num_required_args = self.body.next.head.cnt
-      num_optional_args = 0
-      opt = self.body.next.head.opt
-      while opt do
-        num_optional_args += 1
-        opt = opt.next
-      end
-      num_args = \
-        num_required_args + \
-        num_optional_args + \
-        (rest_arg ? 1 : 0) + \
-        (block_arg ? 1 : 0)
-      return local_vars[0...num_args]
-    else
-      # YARV
-      iseq = self.body.body
-      opt_args = iseq.arg_opt_table
-      opt_args.pop # last arg is a pointer to the start of the code
-      num_args = \
-        iseq.argc + \
-        opt_args.size + \
-        (rest_arg ? 1 : 0) + \
-        (block_arg ? 1 : 0)
-      return local_vars[0...num_args]
-    end
+    return self.body.argument_names
   end
 
   def args_node
-    if self.body.respond_to?(:next)
-      # pre-YARV
-      if self.body.next.class == Node::ARGS then
-        return self.body.next
-      elsif self.body.next.head.class == Node::ARGS then
-        return self.body.next.head
-      else
-        raise "Could not find method arguments"
-      end
-    else
-      # YARV
-      return nil
-    end
+    return self.body.args_node
   end
   private :args_node
 
@@ -112,81 +65,18 @@ module MethodSig
   # that is preceded by an asterisk (*) in the argument list, then
   # return its index, otherwise return nil.
   def rest_arg
-    args_node = args_node()
-    if args_node then
-      # pre-YARV
-      rest = args_node.rest()
-      if rest.class == Node::LASGN then
-        # subtract 2 to account for implicit vars
-        return rest.cnt - 2
-      elsif not rest
-        return nil
-      else
-        return rest > 0 ? rest - 2 : nil
-      end
-    else
-      # YARV
-      rest = self.body.body.arg_rest
-      return rest >= 0 ? rest - 1: nil
-    end
+    return self.body.rest_arg
   end
 
   # If this method has a "block" argument, that is, it has an argument
   # that is preceded by an ampersand (&) in the argument list, then
   # return its index, otherwise return nil.
   def block_arg
-    if self.body.respond_to?(:next) then
-      require 'nodepp'
-      # pre-YARV
-      block = self.body.next
-      if block.class == Node::BLOCK and
-         block.next.head.class == Node::BLOCK_ARG then
-        # subtract 2 to account for implicit vars
-        return block.next.head.cnt - 2
-      else
-        return nil
-      end
-    else
-      # YARV
-      arg_block = self.body.body.arg_block
-      return arg_block >= 0 ? arg_block - 1 : nil
-    end
+    return self.body.block_arg
   end
 
   def set_optional_args(args, args_node, names)
-    if args_node then
-      # pre-YARV
-      opt = args_node.opt
-      while opt do
-        head = opt.head
-        if head.class == Node::LASGN then
-          args[head.vid] = Argument.new(head.vid, head.value.as_expression, head.value, false, false)
-        else
-          raise "Unexpected node type: #{opt.class}"
-        end
-        opt = opt.next
-      end
-    else
-      # YARV
-      iseq = self.body.body
-      opt_pc = iseq.opt_pc
-      env = Nodewrap::ByteDecoder::Environment.new(iseq.local_table())
-      iseq.bytedecode(env, 0, opt_pc)
-      expressions = env.expressions + env.stack
-      expressions.sort!
-      opt_table = self.body.body.arg_opt_table
-      opt_table.pop
-      first_opt_idx =
-        names.size -
-        opt_table.size -
-        (self.rest_arg ? 1 : 0) -
-        (self.block_arg ? 1 : 0)
-      opt_table.each_with_index do |pc, idx|
-        name = names[first_opt_idx + idx]
-        expr = expressions.find { |e| e.pc >= pc }
-        args[name] = Argument.new(name, expr.rhs, nil, false, false) # TODO
-      end
-    end
+    self.body.set_optional_args(args, args_node, names)
   end
   private :set_optional_args
 
@@ -285,6 +175,140 @@ class UnboundMethod
         method_oid().to_s,
         argument_names(),
         arguments())
+  end
+end
+
+class Node
+  # pre-YARV
+  class SCOPE
+    include MethodSig
+
+    def local_vars
+      return self.tbl || []
+    end
+
+    def argument_names
+      local_vars = self.local_vars
+      num_required_args = self.next.head.cnt
+      num_optional_args = 0
+      opt = self.next.head.opt
+      while opt do
+        num_optional_args += 1
+        opt = opt.next
+      end
+      num_args = \
+        num_required_args + \
+        num_optional_args + \
+        (rest_arg ? 1 : 0) + \
+        (block_arg ? 1 : 0)
+      return local_vars[0...num_args]
+    end
+
+    def args_node
+      if self.next.class == Node::ARGS then
+        return self.body.next
+      elsif self.next.head.class == Node::ARGS then
+        return self.next.head
+      else
+        raise "Could not find method arguments"
+      end
+    end
+
+    def rest_arg
+      args_node = args_node()
+      rest = args_node.rest()
+      if rest.class == Node::LASGN then
+        # subtract 2 to account for implicit vars
+        return rest.cnt - 2
+      elsif not rest
+        return nil
+      else
+        return rest > 0 ? rest - 2 : nil
+      end
+    end
+
+    def block_arg
+      block = self.next
+      if block.class == Node::BLOCK and
+         block.next.head.class == Node::BLOCK_ARG then
+        # subtract 2 to account for implicit vars
+        return block.next.head.cnt - 2
+      else
+        return nil
+      end
+    end
+
+    def set_optional_args(args, args_node, names)
+      opt = args_node.opt
+      while opt do
+        head = opt.head
+        if head.class == Node::LASGN then
+          args[head.vid] = Argument.new(head.vid, head.value.as_expression, head.value, false, false)
+        else
+          raise "Unexpected node type: #{opt.class}"
+        end
+        opt = opt.next
+      end
+    end
+  end
+
+  # YARV
+  class METHOD
+    include MethodSig
+
+    def local_vars
+      iseq = self.body
+      local_vars = iseq.local_table
+      return local_vars
+    end
+
+    def argument_names
+      local_vars = self.local_vars
+      iseq = self.body
+      opt_args = iseq.arg_opt_table
+      opt_args.pop # last arg is a pointer to the start of the code
+      num_args = \
+        iseq.argc + \
+        opt_args.size + \
+        (rest_arg ? 1 : 0) + \
+        (block_arg ? 1 : 0)
+      return local_vars[0...num_args]
+    end
+
+    def args_node
+      return nil
+    end
+
+    def rest_arg
+      rest = self.body.arg_rest
+      return rest >= 0 ? rest - 1: nil
+    end
+
+    def block_arg
+      arg_block = self.arg_block
+      return arg_block >= 0 ? arg_block - 1 : nil
+    end
+
+    def set_optional_args(args, args_node, names)
+      iseq = self.body
+      opt_pc = iseq.opt_pc
+      env = Nodewrap::ByteDecoder::Environment.new(iseq.local_table())
+      iseq.bytedecode(env, 0, opt_pc)
+      expressions = env.expressions + env.stack
+      expressions.sort!
+      opt_table = self.body.body.arg_opt_table
+      opt_table.pop
+      first_opt_idx =
+        names.size -
+        opt_table.size -
+        (self.rest_arg ? 1 : 0) -
+        (self.block_arg ? 1 : 0)
+      opt_table.each_with_index do |pc, idx|
+        name = names[first_opt_idx + idx]
+        expr = expressions.find { |e| e.pc >= pc }
+        args[name] = Argument.new(name, expr.rhs, nil, false, false) # TODO
+      end
+    end
   end
 end
 
