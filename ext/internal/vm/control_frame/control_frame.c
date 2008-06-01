a6d0198424d091680ddab9a4ab5c529cc2e58bd9
Paul Brannan <curlypaul924@gmail.com>
2008-05-31 23:17:34 -0400

    Check for valid id before using ID2SYM. Added ControlFrame#prev. Fixed ControlFrame#method_class to actually return method_class instead of method_id.

--- ext/internal/vm/control_frame/control_frame.c
+++ ext/internal/vm/control_frame/control_frame.c
@@ -5,7 +5,18 @@
 
 #include "vm_core.h"
 
+/* TODO: Using this class is dangerous, because the control frame can be
+ * popped but a reference kept around to it.  It will still point to
+ * valid memory, but the data itself will be invalid.
+ */
+
 static VALUE rb_cVmControlFrame;
+
+void mark_ruby_internal_control_frame(
+    struct RubyInternalControlFrame * cfp)
+{
+  rb_gc_mark(cfp->thread);
+}
 
 static VALUE control_frame_iseq(VALUE control_frame)
 {
@@ -46,14 +57,61 @@ static VALUE control_frame_method_id(VALUE control_frame)
 {
   struct RubyInternalControlFrame * cfp;
   Data_Get_Struct(control_frame, struct RubyInternalControlFrame, cfp);
-  return ID2SYM(cfp->control_frame->method_id);
+  if(cfp->control_frame->method_id)
+  {
+    return ID2SYM(cfp->control_frame->method_id);
+  }
+  else
+  {
+    return Qnil;
+  }
 }
 
 static VALUE control_frame_method_class(VALUE control_frame)
 {
   struct RubyInternalControlFrame * cfp;
   Data_Get_Struct(control_frame, struct RubyInternalControlFrame, cfp);
-  return ID2SYM(cfp->control_frame->method_id);
+  return cfp->control_frame->method_class;
+}
+
+static VALUE control_frame_prev(VALUE control_frame)
+{
+  struct RubyInternalControlFrame * cfp;
+  rb_thread_t * th;
+  rb_control_frame_t * prev_cfp;
+
+  Data_Get_Struct(control_frame, struct RubyInternalControlFrame, cfp);
+
+  GetThreadPtr(cfp->thread, th);
+
+  prev_cfp = RUBY_VM_PREVIOUS_CONTROL_FRAME(cfp->control_frame);
+
+  printf("current: %p\n", cfp->control_frame);
+  printf("prev: %p\n", prev_cfp);
+  printf("th->stack: %p\n", th->stack);
+  printf("th->stack + th->stack_size: %p\n", th->stack + th->stack_size);
+
+  if((void *)(th->stack + th->stack_size) == (void *)prev_cfp)
+  {
+    return Qnil;
+  }
+  else
+  {
+    VALUE cfp_v;
+    struct RubyInternalControlFrame * new_cfp;
+
+    cfp_v = Data_Make_Struct(
+        rb_cVmControlFrame,
+        struct RubyInternalControlFrame,
+        mark_ruby_internal_control_frame,
+        free,
+        new_cfp);
+
+    new_cfp->control_frame = prev_cfp;
+    new_cfp->thread = cfp->thread;
+
+    return cfp_v;
+  }
 }
 
 #endif
@@ -69,6 +127,7 @@ void Init_control_frame(void)
   rb_define_method(rb_cVmControlFrame, "proc", control_frame_proc, 0);
   rb_define_method(rb_cVmControlFrame, "method_id", control_frame_method_id, 0);
   rb_define_method(rb_cVmControlFrame, "method_class", control_frame_method_class, 0);
+  rb_define_method(rb_cVmControlFrame, "prev", control_frame_prev, 0);
 #endif
 }
 
