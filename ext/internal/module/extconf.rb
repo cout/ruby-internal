037afab44fc30f6f74f75a0891d373b1ba8d0965
Paul Brannan <curlypaul924@gmail.com>
2008-09-24 17:08:11 -0400

    Use vm_get_ruby_level_next_cfp instead of vm_get_ruby_level_cfp.

--- ext/internal/module/extconf.rb
+++ ext/internal/module/extconf.rb
@@ -1,5 +1,10 @@
 $: << '../..'
 require 'mkmf-ruby-internal'
+
+have_func('vm_get_ruby_level_cfp')
+have_func('vm_get_ruby_level_next_cfp')
+
 ruby_version_code = RUBY_VERSION.gsub(/\./, '').to_i
 $CPPFLAGS << " -DRUBY_VERSION_CODE=#{ruby_version_code}"
+
 create_ruby_internal_makefile 'internal/module/module'
