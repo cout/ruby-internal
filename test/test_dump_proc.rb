ad5147382d1b02eb0e770e4200b6b9a316ea4f23
Paul Brannan <curlypaul924@gmail.com>
2008-09-27 09:08:34 -0400

    Removed debugging output.

--- test/test_dump_proc.rb
+++ test/test_dump_proc.rb
@@ -78,9 +78,6 @@ class TC_Dump_Proc < Test::Unit::TestCase
 
   Proc_Node_Samples.each do |node_name, sample_code|
     p = proc {
-      puts "///"
-      puts sample_code
-      puts "\\\\\\"
       p_orig = eval <<-END_DEF
         proc {
           #{sample_code}
@@ -92,15 +89,6 @@ class TC_Dump_Proc < Test::Unit::TestCase
       #puts "loading"
       u = Marshal.load(d)
       p_new = u.bind(binding)
-
-      puts "*** ORIG ***"
-      p p_orig.body
-      p p_orig
-      puts p_orig.body.disasm
-      puts "*** NEW ***"
-      p p_new.body
-      p p_new
-      puts p_new.body.disasm
 
       orig_result = nil
       orig_exc = nil
