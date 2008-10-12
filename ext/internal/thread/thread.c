4c8194e5f68a563514199a775979b95a31d30848
Paul Brannan <curlypaul924@gmail.com>
2008-10-12 19:14:20 -0400

    Added Thread#exec_tag().

--- ext/internal/thread/thread.c
+++ ext/internal/thread/thread.c
@@ -2,6 +2,7 @@
 
 #ifdef RUBY_VM
 #include "vm_core.h"
+#include "eval_intern.h"
 #include "internal/vm/control_frame/control_frame.h"
 #endif
 
@@ -49,6 +50,18 @@ static VALUE thread_cfp(VALUE self)
   }
 }
 
+int ruby_exec_tag()
+{
+  /* TODO: EXEC_TAG not available on 1.8.x */
+  rb_thread_t * const _th = GET_THREAD();
+  return EXEC_TAG();
+}
+
+VALUE ruby_f_exec_tag(VALUE self)
+{
+  return INT2NUM(ruby_exec_tag());
+}
+
 #endif
 
 void Init_thread(void)
@@ -72,6 +85,7 @@ void Init_thread(void)
   rb_cVmControlFrame = rb_const_get(rb_cRubyVM, rb_intern("ControlFrame"));
 
   rb_define_method(rb_cThread, "cfp", thread_cfp, 0);
+  rb_define_method(rb_cThread, "exec_tag", ruby_f_exec_tag, 0);
 #endif
 }
 
