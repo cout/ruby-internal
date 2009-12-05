99d198fb6fc1c0173ed7a9b8cda04184e8c0b68b
Paul Brannan <cout@sapphire.(none)>
2009-12-04 23:08:20 -0500

    Added an error message.

--- ext/generate_cached.sh
+++ ext/generate_cached.sh
@@ -2,6 +2,12 @@
 
 dir="$1"
 
+if [ -z "$dir" ]; then
+  echo "Usage: $0 <dir>"
+  echo "where <dir> is the directory where the untarred ruby source archives can be found."
+  exit 1
+fi
+
 for f in "$dir"/ruby-?.?.?; do
   ruby generate_cached.rb "$f"
 done
